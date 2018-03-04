3ae9ff26b8071f930a1281e0e57ff66c6f0ed11e
Serhii <sergey.petrunya@gmail.com>
2018-03-04 23:17:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var theBeatlesPlay = (musicians, instruments) =>{
-  var arr = [].concat(musicians, instruments)
+  var arr = []
+  arr.concat(musicians, instruments)
   return arr.reduce(function(musicians,instruments){
     musicians + ' plays ' + instruments
   })
