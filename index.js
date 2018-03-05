30e5c24702621b6788c59133919d6566b3033739
Serhii <sergey.petrunya@gmail.com>
2018-03-05 01:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ var theBeatlesPlay = (musicians, instruments) =>{
     return musicians.map(function (subMus){
       return instruments.map(function (subInst){
         return subMus + " plays " + subInst
-
-// arr.concat(musicians, instruments)
-// return arr.reduce(function(musicians,instruments){
-//   musicians + ' plays ' + instruments
-// })
