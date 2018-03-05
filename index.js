de82691141edb47657d5bf9fbb47c50a29cc7c01
Serhii <sergey.petrunya@gmail.com>
2018-03-05 02:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var theBeatlesPlay = (musicians, instruments) => {
-    return musicians.map(function(subMus){
-      return instruments.map(function(subInst){
-          return subMus + " plays " + subInst;
-      });
-    });
+  for(let i = 0; i < musicians.length; i++ ){
+    var inner = musicians[i]; 
+    for(let j = 0; j < instruments.length; j++ ){
+      var outer = instruments[i];
+    } 
+  }  
+  return inner  + " plays " + outer
 }
+
+// return musicians.map(function(subMus){
+//   return instruments.map(function(subInst){
+//       return subMus + " plays " + subInst;
+//   });
+// });
